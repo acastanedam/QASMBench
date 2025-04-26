acf33daf24c75426bc12fa25a33716eb3923e22e
Arcesio Castaneda Medina <arcesio.castaneda.medina@itwm.fraunhofer.de>
2025-04-26 16:44:08 +0200

    Add main to metricplotter, small changes

--- metrics/metric_plotter.py
+++ metrics/metric_plotter.py
@@ -9,12 +9,12 @@
 # ----------------------------------------------------------------------
 
 
-from typing import *
-import numpy as np
-import matplotlib.pyplot as plt
-import glob
-import qiskit
 import os
+from typing import *
+
+import matplotlib.pyplot as plt
+import qiskit
+
 from metrics.QMetric import QMetric
 
 
@@ -30,17 +30,25 @@ class MetricPlotter:
     """
 
     def __init__(self, custom_keys: list[str] = None, file_path: str = "."):
-        self.QASMBENCH_KEYS: list[str] = ["circuit_depth", "circuit_width", "retention_lifespan",
-                                          "gate_density", "measurement_density", "entanglement_variance"]
-        self.SUPERMARQ_KEYS: list[str] = ["communication_supermarq", "measurement_supermarq", "depth_supermarq",
-                                          "parallelism_supermarq", "entanglement_supermarq", "liveness_supermarq"]
+        self.QASMBENCH_KEYS: list[str] = [
+            "circuit_depth",
+            "circuit_width",
+            "retention_lifespan",
+            "gate_density",
+            "measurement_density",
+            "entanglement_variance",
+        ]
+        self.SUPERMARQ_KEYS: list[str] = [
+            "communication_supermarq",
+            "measurement_supermarq",
+            "depth_supermarq",
+            "parallelism_supermarq",
+            "entanglement_supermarq",
+            "liveness_supermarq",
+        ]
         self.CUSTOM_KEYS: list[str] = custom_keys
-        self.PROCESSED_FEATURES = {"SMALL": [],
-                                   "MEDIUM": [],
-                                   "LARGE": []}
-        self.CIRCUIT_SIZES = {"SMALL": 10,
-                              "MEDIUM": 28,
-                              "LARGE": 433}
+        self.PROCESSED_FEATURES = {"SMALL": [], "MEDIUM": [], "LARGE": []}
+        self.CIRCUIT_SIZES = {"SMALL": 10, "MEDIUM": 28, "LARGE": 433}
         self.SIZE_KEYS = ["SMALL", "MEDIUM", "LARGE"]
         self.file_path = file_path
         self.files = []
@@ -62,7 +70,7 @@ class MetricPlotter:
         """
         for root, dirs, files in os.walk(self.file_path):
             for file in files:
-                if file.endswith(".qasm"):
+                if file.endswith(".qasm") and ("transpiled" not in file):
                     self.files.append(os.path.join(root, file))
 
     def load_next_qasm_to_circuit(self) -> qiskit.QuantumCircuit:
@@ -72,7 +80,7 @@ class MetricPlotter:
         """
         assert len(self.files) > 0
         for file in self.files:
-            circuit = qiskit.QuantumCircuit().from_qasm_file(file)
+            circuit = qiskit.QuantumCircuit.from_qasm_file(file)
             yield circuit
 
     def load_next_qasm(self) -> str:
@@ -82,13 +90,15 @@ class MetricPlotter:
         """
         assert len(self.files) > 0
         for file in self.files:
-            with open(file, 'r') as f:
+            with open(file, "r") as f:
                 try:
                     qasm = f.read()
                 except:
-                    print(f"{file} failed to evaluate - Problem with loading QASM. Skipping to next.")
+                    print(
+                        f"{file} failed to evaluate - Problem with loading QASM. Skipping to next."
+                    )
                     continue
-            circuit_name = file.split('/')[-1].rstrip('qasm.')
+            circuit_name = file.split("/")[-1].rstrip("qasm.")
             yield qasm, circuit_name
 
     def process_qasm_files(self) -> None:
@@ -99,10 +109,11 @@ class MetricPlotter:
         """
         qasm_files = self.load_next_qasm()
         for qasm, circuit_name in qasm_files:
+            print(circuit_name.upper())
             feature_dict = QMetric(qasm).evaluate_qasm()
-            if feature_dict['qubit_count'] <= self.CIRCUIT_SIZES["SMALL"]:
+            if feature_dict["qubit_count"] <= self.CIRCUIT_SIZES["SMALL"]:
                 self.PROCESSED_FEATURES["SMALL"].append({circuit_name: feature_dict})
-            elif feature_dict['qubit_count'] <= self.CIRCUIT_SIZES["MEDIUM"]:
+            elif feature_dict["qubit_count"] <= self.CIRCUIT_SIZES["MEDIUM"]:
                 self.PROCESSED_FEATURES["MEDIUM"].append({circuit_name: feature_dict})
             else:
                 self.PROCESSED_FEATURES["LARGE"].append({circuit_name: feature_dict})
@@ -142,12 +153,16 @@ class MetricPlotter:
                     peak = circuit[circuit_name][feature]
         return peak
 
-    def plot_feature(self, feature, figsize=None,
-                     bar_width=1,
-                     plot_limit=None,
-                     scale_factor=1.1,
-                     filepath=None,
-                     color_palette=plt.cm.tab20c.colors):
+    def plot_feature(
+        self,
+        feature,
+        figsize=None,
+        bar_width=1,
+        plot_limit=None,
+        scale_factor=1.1,
+        filepath=None,
+        color_palette=plt.cm.tab20c.colors,
+    ):
         """
         :param feature: The target feature to be plotted. Must exist under the self.PROCESSED_FEATURES tag.
         :param figsize: Figure size to be set before plotting. If left as None, it will default to 10,20
@@ -177,7 +192,12 @@ class MetricPlotter:
             for result in self.PROCESSED_FEATURES[key]:
                 has_size = True
                 circuit_name = list(result.keys())[0]
-                plt.bar(pos, result[circuit_name][feature], width=bar_width, color=color_palette[clr_index])
+                plt.bar(
+                    pos,
+                    result[circuit_name][feature],
+                    width=bar_width,
+                    color=color_palette[clr_index],
+                )
                 clr_index += 1
                 if clr_index >= len(color_palette):
                     clr_index = 0
@@ -185,19 +205,40 @@ class MetricPlotter:
                 circuit_names.append(self.format_circuit_name(circuit_name))
                 pos += bar_width
             if key != "LARGE" and has_size:
-                plt.vlines(pos, 0, plot_limit, linewidth=2, color='black')
+                plt.vlines(pos, 0, plot_limit, linewidth=2, color="black")
                 vline_position.append(pos)
                 pos += bar_width
-                plt.text(x=-0.5 * bar_width + (vline_position[-2] + vline_position[-1]) / 2,
-                         y=0.9 * plot_limit, s=key, fontweight="bold")
+                plt.text(
+                    x=-0.5 * bar_width + (vline_position[-2] + vline_position[-1]) / 2,
+                    y=0.9 * plot_limit,
+                    s=key,
+                    fontweight="bold",
+                )
             elif key == "LARGE" and has_size:
                 vline_position.append(pos)
-                plt.text(x=-0.5 * bar_width + (vline_position[-2] + vline_position[-1]) / 2,
-                         y=0.9 * plot_limit, s=key, fontweight="bold")
+                plt.text(
+                    x=-0.5 * bar_width + (vline_position[-2] + vline_position[-1]) / 2,
+                    y=0.9 * plot_limit,
+                    s=key,
+                    fontweight="bold",
+                )
         plt.xticks(circuit_pos, circuit_names, rotation=90)
         plt.ylim(0, plot_limit)
         plt.xlim(0, circuit_pos[-1] + 0.5 * bar_width)
-        plt.grid(axis='y')
+        plt.grid(axis="y")
         plt.ylabel(self.format_feature_name(feature))
         if filepath is not None:
             plt.savefig(filepath)
+        else:
+            plt.show()
+
+
+if __name__ == "__main__":
+    size = "small"
+    feature = "gate_density"
+
+    metric = MetricPlotter(file_path=size)
+    metric.search_for_qasm()
+    metric.process_qasm_files()
+
+    metric.plot_feature(feature, filepath=f"./{size}_{feature}.png")
