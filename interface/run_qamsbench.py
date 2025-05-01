05ba2a9c91a629b1a212592c685dafb580118a7b
Arcesio Castaneda Medina <arcesio.castaneda.medina@itwm.fraunhofer.de>
2025-05-01 19:21:07 +0200

    Parallellize over providers/backends

--- interface/run_qamsbench.py
+++ interface/run_qamsbench.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import os
 
 from qasmbenchmark import QASMBenchmark
@@ -8,13 +9,13 @@ _pwd = os.getcwd()
 path = _pwd
 
 # selected category for QASMBench
-category = "small"
+category = "programs/small"
 
 # select only the circuits with the number of qubits in the list
-num_qubits_list = list(range(3, 4))
+num_qubits_list = list(range(3, 5))
 
 # whether to remove the final measurement in the circuit
-remove_final_measurements = True
+remove_final_measurements = False
 
 # whether use qiskit.transpile() to transpile the circuits (note: must provide qiskit backend)
 do_transpile = True
@@ -26,16 +27,21 @@ OPTIMIZATION_LEVEL = 2
 EXECUTION_TYPE = "off"
 PLOT_OUTPUT = True
 
-providers_backends = {
+PROVIDERS_BACKENDS = {
     "aqt": "offline_simulator_noise",
     "ibmq": "fake_lima",
-    "iqm": "fake_deneb",
+    "iqm": "fake_apollo",
 }
 
-for provider, backend in providers_backends.items():
-    output_dir = f"compiled/{provider}_{backend}/{category}"
+# max workers
+MAX_WORKERS = 3
+
+
+def process_provider_backend(provider, backend):
+    output_dir = f"{path}/compiled/{provider}_{backend}/{category}"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+    original_dir = os.getcwd()
     os.chdir(output_dir)
 
     transpile_args = {
@@ -62,4 +68,14 @@ for provider, backend in providers_backends.items():
     for _circuit_name in bm.circ_name_list:
         _circuit = bm.get(_circuit_name)
 
-    os.chdir(_pwd)
+    os.chdir(original_dir)
+
+
+with concurrent.futures.ProcessPoolExecutor(MAX_WORKERS) as executor:
+    list(
+        executor.map(
+            process_provider_backend,
+            PROVIDERS_BACKENDS.keys(),
+            PROVIDERS_BACKENDS.values(),
+        )
+    )
